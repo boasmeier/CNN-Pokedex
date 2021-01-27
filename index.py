32123d7f6afbe4e341c8923046525673341f23a4
boasmeier <62314872+boasmeier@users.noreply.github.com>
2021-01-27 10:13:02 +0100

    new: added clasification to UI

--- index.py
+++ index.py
@@ -1,50 +1,37 @@
 import sys
 import random
-
 from PySide6 import QtCore, QtWidgets, QtGui
-
-
+import classify
 
 class MyWidget(QtWidgets.QWidget):
     def __init__(self):
         super().__init__()
 
-        self.hello = ["Hallo Welt", "Hei maailma", "Hola Mundo", "Привет мир"]
-        self.button = QtWidgets.QPushButton("Click me!")
         self.buttonImg = QtWidgets.QPushButton("Chose img!")
         self.text = QtWidgets.QLabel("Pokedex!", alignment=QtCore.Qt.AlignTop.AlignHCenter)
-        self.img = QtWidgets.QLabel(self)
+        self.img = QtWidgets.QLabel(self, alignment=QtCore.Qt.AlignVCenter.AlignHCenter)
 
-        self.initFileDialog()
         self.initLayout()
-        
-        self.button.clicked.connect(self.magic)
         self.buttonImg.clicked.connect(self.openFileDialog)
-
-    @QtCore.Slot()
-    def magic(self):
-        self.text.setText(random.choice(self.hello))
 
     def initLayout(self):
         self.layout = QtWidgets.QVBoxLayout()
         self.layout.addWidget(self.text)
         self.layout.addWidget(self.img)
-        self.layout.addWidget(self.button)
         self.layout.addWidget(self.buttonImg)
         self.setLayout(self.layout)
-
-    def initFileDialog(self):
-        self.dialog = QtWidgets.QFileDialog(self)
-        self.dialog.setFileMode(QtWidgets.QFileDialog.AnyFile)
-        self.dialog.setViewMode(QtWidgets.QFileDialog.Detail)
-        self.dialog.setNameFilter("Images (*.png *.xpm *.jpg)")
 
     def openFileDialog(self):
         filePath, _ = QtWidgets.QFileDialog.getOpenFileName(self, "Open File", "./", "Images (*.png *.xpm *.jpg)")
         if(filePath):
-            print(filePath)
-            self.pixmap = QtGui.QPixmap(filePath)
-            self.img.setPixmap(self.pixmap)
+            self.showClassification(filePath)
+
+    def showClassification(self, filePath):
+        print(filePath)
+        self.pixmap = QtGui.QPixmap(filePath)
+        self.img.setPixmap(self.pixmap)
+        classifiedImg = classify.classifyImg("pokedex.model", "lb.pickle", filePath)
+        self.img.setPixmap(classifiedImg)
 
 
 if __name__ == "__main__":
