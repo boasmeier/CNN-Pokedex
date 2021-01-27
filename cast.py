7728c96569866501b7c7a0cddfef6890870ae066
boasmeier <62314872+boasmeier@users.noreply.github.com>
2021-01-27 11:08:33 +0100

    new: added download

---
+++ cast.py
@@ -0,0 +1,18 @@
+from PySide6 import QtGui, QtCore
+
+class QPixmap2QByteArray(object):
+    def __call__(self, q_image: QtGui.QImage) -> QtCore.QByteArray:
+        """
+            Args:
+                 q_image: QImage to be converted to byte stream.
+            Returns:
+                                   The byte array converted from q_image.
+        """
+                 # Get an empty byte array
+        byte_array = QtCore.QByteArray()
+                 # Bind the byte array to the output stream
+        buffer = QtCore.QBuffer(byte_array)
+        buffer.open(QtCore.QIODevice.WriteOnly)
+                 # Save the data in png format
+        q_image.save(buffer, "png", quality=100)
+        return byte_array
--- index.py
+++ index.py
@@ -2,23 +2,28 @@ import sys
 import random
 from PySide6 import QtCore, QtWidgets, QtGui
 import classify
+import cast
 
 class MyWidget(QtWidgets.QWidget):
     def __init__(self):
         super().__init__()
 
         self.buttonImg = QtWidgets.QPushButton("Chose img!")
+        self.buttonDownload = QtWidgets.QPushButton("Download")
         self.text = QtWidgets.QLabel("Pokedex!", alignment=QtCore.Qt.AlignTop.AlignHCenter)
         self.img = QtWidgets.QLabel(self, alignment=QtCore.Qt.AlignVCenter.AlignHCenter)
+        #self.image = None
 
         self.initLayout()
         self.buttonImg.clicked.connect(self.openFileDialog)
+        self.buttonDownload.clicked.connect(self.download)
 
     def initLayout(self):
         self.layout = QtWidgets.QVBoxLayout()
         self.layout.addWidget(self.text)
         self.layout.addWidget(self.img)
         self.layout.addWidget(self.buttonImg)
+        self.layout.addWidget(self.buttonDownload)
         self.setLayout(self.layout)
 
     def openFileDialog(self):
@@ -28,12 +33,15 @@ class MyWidget(QtWidgets.QWidget):
 
     def showClassification(self, filePath):
         print(filePath)
-        self.pixmap = QtGui.QPixmap(filePath)
-        self.img.setPixmap(self.pixmap)
         classifiedImg = classify.classifyImg("pokedex.model", "lb.pickle", filePath)
-        image = QtGui.QImage(classifiedImg, classifiedImg.shape[1], classifiedImg.shape[0], classifiedImg.shape[1] * 3, QtGui.QImage.Format_RGB888).rgbSwapped()
-        pixmap = QtGui.QPixmap(image)
+        self.image = QtGui.QImage(classifiedImg, classifiedImg.shape[1], classifiedImg.shape[0], classifiedImg.shape[1] * 3, QtGui.QImage.Format_RGB888).rgbSwapped()
+        pixmap = QtGui.QPixmap(self.image)
         self.img.setPixmap(pixmap)
+
+    def download(self):
+        if(self.image):
+            converter = cast.QPixmap2QByteArray()
+            QtWidgets.QFileDialog.saveFileContent(converter(self.image), "classified.jpg")
 
 
 if __name__ == "__main__":
